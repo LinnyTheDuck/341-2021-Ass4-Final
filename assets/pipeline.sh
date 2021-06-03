103a8c15ef3226284150d3a847480cbc748e83fc
stefenie <stef.linyi@gmail.com>
2021-06-04 01:31:53 +1200

    break-test-2

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -5,7 +5,11 @@
 # fi
 # echo 'config check passed!'
 #npm install
-npm run build
+echo npm run build
+if [npm run build = 1]; then
+    echo 'build failed'
+    exit 2
+fi
 # cd src/__tests__
 # npm run test
 # cd ../..
