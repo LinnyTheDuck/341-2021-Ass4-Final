d7fb7ce73d79c304280aa27b1e7ff6948c8b3f22
stefenie <stef.linyi@gmail.com>
2021-06-04 02:27:39 +1200

    test-6

--- assets/src/__tests__/pastpurchases.test.tsx
+++ assets/src/__tests__/pastpurchases.test.tsx
@@ -12,6 +12,16 @@ it("Renders Without Crashing", () => {
     shallow(<PastPurchase />);
 });
 
+describe("Display Welcome Message Test", () => {
+    test("Welcome Message Render Should Be: ‘Hello user@emailaddress.com!’", () => {
+        const email = "user@emailaddress.com"; // set an email to pass through
+        const wrapper = shallow(<PastPurchase/>); // render the past purchases
+        wrapper.setState({userInfo: {attributes: {email: "user@emailaddress.com"}}}); // set the email in the state
+        const welcome = <h3>{`Hello ${email}!`}</h3>; // put email in the heading
+        expect(wrapper.contains(welcome)).toEqual(true); // check if match
+    })
+})
+
 describe("Past Purchases Title Test", () => {
     test("Past Purchases Render Heading Should Be: ‘Past purchases’", () => {
         const heading = <h3>Past purchases</h3>; // define what the heading should be
